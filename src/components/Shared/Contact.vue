03bb2d3bf5fad26efbe8c9d473c63a5cd76f490d
Salem bahattab <127092764+salembht@users.noreply.github.com>
2025-07-07 20:31:33 +0300

    edit conact

--- src/components/Shared/Contact.vue
+++ src/components/Shared/Contact.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-const { t ,locale} = useI18n()
+const { t, locale } = useI18n()
 </script>
 
 <template>
@@ -21,7 +21,7 @@ const { t ,locale} = useI18n()
                 <div class="w-full lg:w-1/2 bg-[#9C0B26] text-white rounded-2xl p-8 shadow-xl">
                     <h3 class="text-2xl font-bold mb-8 relative pb-4">
                         {{ t('contact.info_heading') }}
-                         <div class="absolute bottom-0 w-10 h-1 bg-[#D6A756]"
+                        <div class="absolute bottom-0 w-10 h-1 bg-[#D6A756]"
                             :class="locale === 'ar' ? 'right-0' : 'left-0'"></div>
                     </h3>
 
@@ -35,11 +35,12 @@ const { t ,locale} = useI18n()
                             <div>
                                 <h4 class="font-bold text-lg">{{ t('contact.phone_label') }}</h4>
                                 <p class="mt-1">
-                                    <a :href="`https://wa.me/${t('contact.phone_value').replace(/[^\d]/g, '')}`"
+                                    <!-- <a :href="`https://wa.me/${t('contact.phone_value').replace(/[^\d]/g, '')}`"
                                         target="_blank"
-                                        class="hover:underline transition-all  duration-300 hover:text-[#D6A756]" dir="ltr">
+                                        class="hover:underline transition-all  duration-300 hover:text-[#D6A756]"
+                                        dir="ltr">
                                         {{ t('contact.phone_value') }}
-                                    </a>
+                                    </a> -->
                                 </p>
                             </div>
                         </div>
@@ -53,10 +54,10 @@ const { t ,locale} = useI18n()
                             <div>
                                 <h4 class="font-bold text-lg">{{ t('contact.email_label') }}</h4>
                                 <p class="mt-1">
-                                    <a :href="`mailto:${t('contact.email_value')}`"
+                                    <!-- <a :href="`mailto:${t('contact.email_value')}`"
                                         class="hover:underline transition-all duration-300 hover:text-[#D6A756]">
                                         {{ t('contact.email_value') }}
-                                    </a>
+                                    </a> -->
                                 </p>
                             </div>
                         </div>
@@ -99,7 +100,8 @@ const { t ,locale} = useI18n()
                         </iframe>
 
                         <div class="absolute bottom-4 right-4 bg-white rounded-lg shadow-md py-2 px-4">
-                            <a href="https://www.google.com/maps/place/JESA6619,+6619+Abu+Al+Fadl+Al+Mouhsi,+2603,+As+Salamah+District,+Jeddah+23436,+Saudi+Arabia/@21.5802697,39.1470876,18z/data=!3m1!4b1!4m10!1m2!2m1!1z2K3ZiiDYp9mE2LPZhNin2YXYqSAtINij2KjZiCDYp9mE2YHYttmEINin2YTZhdit2LPZiiA2NjE5INis2K_YqSDYjDIzNDM2INin2YTZhdmF2YTZg9ipINin2YTYudix2KjZitipINin2YTYs9i52YjYr9mK2Kkg4oCTINmH2KfYqtmBIDAwOTY2NTA1MTQ0NiDYsy7YqiA0MDMwNTk1MzM4!3m6!1s0x15c3da848293561b:0x3e8c805069fa976e!8m2!3d21.5802697!4d39.1490991!15sCqIB2K3ZiiDYp9mE2LPZhNin2YXYqSAtINij2KjZiCDYp9mE2YHYttmEINin2YTZhdit2LPZiiA2NjE5INis2K_YqSDYjDIzNDM2INin2YTZhdmF2YTZg9ipINin2YTYudix2KjZitipINin2YTYs9i52YjYr9mK2Kkg4oCTINmH2KfYqtmBIDAwOTY2NTA1MTQ0NiDYsy7YqiA0MDMwNTk1MzM4kgERY29tcG91bmRfYnVpbGRpbmfgAQA!16s%2Fg%2F11k6bby4xc?entry=ttu&g_ep=EgoyMDI1MDYyOS4wIKXMDSoASAFQAw%3D%3D" target="_blank"
+                            <a href="https://www.google.com/maps/place/JESA6619,+6619+Abu+Al+Fadl+Al+Mouhsi,+2603,+As+Salamah+District,+Jeddah+23436,+Saudi+Arabia/@21.5802697,39.1470876,18z/data=!3m1!4b1!4m10!1m2!2m1!1z2K3ZiiDYp9mE2LPZhNin2YXYqSAtINij2KjZiCDYp9mE2YHYttmEINin2YTZhdit2LPZiiA2NjE5INis2K_YqSDYjDIzNDM2INin2YTZhdmF2YTZg9ipINin2YTYudix2KjZitipINin2YTYs9i52YjYr9mK2Kkg4oCTINmH2KfYqtmBIDAwOTY2NTA1MTQ0NiDYsy7YqiA0MDMwNTk1MzM4!3m6!1s0x15c3da848293561b:0x3e8c805069fa976e!8m2!3d21.5802697!4d39.1490991!15sCqIB2K3ZiiDYp9mE2LPZhNin2YXYqSAtINij2KjZiCDYp9mE2YHYttmEINin2YTZhdit2LPZiiA2NjE5INis2K_YqSDYjDIzNDM2INin2YTZhdmF2YTZg9ipINin2YTYudix2KjZitipINin2YTYs9i52YjYr9mK2Kkg4oCTINmH2KfYqtmBIDAwOTY2NTA1MTQ0NiDYsy7YqiA0MDMwNTk1MzM4kgERY29tcG91bmRfYnVpbGRpbmfgAQA!16s%2Fg%2F11k6bby4xc?entry=ttu&g_ep=EgoyMDI1MDYyOS4wIKXMDSoASAFQAw%3D%3D"
+                                target="_blank"
                                 class="text-[#9C0B26] font-bold flex items-center hover:text-[#D6A756] transition-colors">
                                 <i class="fas fa-directions mr-2"></i>
                                 {{ t('contact.directions') }}
